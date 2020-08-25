d3029337c892ad1b30d91522940c8970d39347d4
onatsko <onatsko@gmail.com>
2020-08-25 22:39:27 +0300

    init commit

---
+++ SqlBackupToGoogleDrive/SqlBackupToGoogleDrive.cs
@@ -0,0 +1,22 @@
+﻿namespace SqlBackupToGoogleDrive
+{
+    public class SqlBackupToGoogleDrive
+    {
+        private string _connectionString;
+
+        public SqlBackupToGoogleDrive(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public BackupResult CreateBackup()
+        {
+            return new BackupResult(false, "need to do");
+        }
+
+        public SendResult SendToGoogleDrive(string token, string file)
+        {
+            return new SendResult(false, "need to do");
+        }
+    }
+}
